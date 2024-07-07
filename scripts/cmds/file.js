b0102d8ce72ae6e2ea57c418d9c052b8ac249659
khlea-ishere01 <khleajazz@gmail.com>
2024-07-08 01:02:51 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100053549552408",];
+    const permission = ["61561979464493",];
     if (!permission.includes(event.senderID)) {
       return api.sendMessage("- Paku, You don't have permission to use this command. 🐤", event.threadID, event.messageID);
     }
