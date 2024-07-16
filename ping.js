04d2b846102b24543a4ff8a25402def31d106ca3
khlea-ishere01 <khleajazz@gmail.com>
2024-07-16 17:10:37 +0800

    Update

---
+++ ping.js
@@ -0,0 +1,15 @@
+const config = require("../data/config.dev.json")
+module.exports.run = async (bot, message, args) => {
+    if (message.author.id == config.ownerid) {
+        const m = await message.channel.send('Pong!');
+        m.edit(`:ping_pong: Pong! \`${m.createdTimestamp - message.createdTimestamp}ms\``);
+    }
+    return;
+}; //stolen from https://github.com/RayzrDev/RayzrDevBot.git
+
+module.exports.help = {
+    name: 'ping',
+    usage: 'ping',
+    type: "Private",
+    description: 'Pings the bot. Restricted to the bot owner.'
+};
